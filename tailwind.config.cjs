dd35c0d189f94d17a1f7b2fa58765ef562a01982
Cleidison Silva <cleidison.dev@gmail.com>
2023-02-27 09:57:30 -0300

    feat: Add custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.tsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
